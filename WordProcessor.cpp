40993951d016055c479e7e466c68352a1c2705b5
Michael L. Collard <collard@uakron.edu>
2021-03-30 07:26:50 -0400

    Remove file-level attempt at map

--- WordProcessor.cpp
+++ WordProcessor.cpp
@@ -10,30 +10,6 @@
 #include <locale>
 #include <memory>
 
-namespace {
-
-const auto temp = std::ctype<char>::classic_table();
-std::vector<std::ctype<char>::mask> bar(temp, temp + std::ctype<char>::table_size);
-
-// bar[' '] ^= ctype_base::space;
-// bar['\t'] &= ~(ctype_base::space | ctype_base::cntrl);
-// bar[':'] |= ctype_base::space;
-
-// struct word_delimiter : std::ctype<char> {
-//     word_delimiter()
-//         : std::ctype<char>(get_table())
-//     {}
-
-//     static mask const* get_table() {
-//         static mask rc[table_size];
-//         rc[':'] = std::ctype_base::space;
-//         rc['\n'] = std::ctype_base::space;
-//         return &rc[0];
-//     }
-// };
-
-}
-
 // constructor with input stream and processWord function
 WordProcessor::WordProcessor(std::istream& in, std::function<bool(const std::string& word)> processWord)
     : input(in), processWord(processWord)
